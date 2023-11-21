46034e373d0b04d4f845104658e783a13d998129
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-11-21 07:24:34 -0800

    Update Apps3.js
    Chapter 26 to 30 Assignement

--- Apps3.js
+++ Apps3.js
@@ -1054,3 +1054,85 @@
 // }
 // console.log("word 'the' is " + count + " times in a sentence." )
 
+
+// XX ====================XX=================XX=====================XX=============
+//                               CHAPTER 26 to 30 
+// XX ====================XX=================XX=====================XX=============
+
+// ============= QUESTION 01 
+
+// let input = +prompt("write a decimal number")
+
+// let iuput_round = Math.round(input)
+// let iuput_floor = Math.floor(input)
+// let iuput_ceill = Math.ceil(input)
+
+// console.log(iuput_round + ' ==> round ')
+// console.log(iuput_floor + ' ==> floor ')
+// console.log(iuput_ceill + ' ==> ceil ')
+
+
+// ============= QUESTION 02
+
+// let input2 = +prompt("write a negetive decimal number")
+
+// let iuput2_round = Math.round(input2)
+// let iuput2_floor = Math.floor(input2)
+// let iuput2_ceill = Math.ceil(input2)
+
+// console.log(iuput2_round + ' ==> round ')
+// console.log(iuput2_floor + ' ==> floor ')
+// console.log(iuput2_ceill + ' ==> ceil ')
+
+
+// QUESTION 03
+
+// let num = +prompt("write a negetive number")
+// let absolute = Math.abs(num)
+// console.log(absolute)
+
+
+// ============= QUESTION 04
+
+// function rollDice() {
+//     let dice = Math.floor(Math.random() * 6) + 1;
+
+//     document.getElementById('dice').innerText = dice;
+// }
+// document.getElementById('dice').innerText = 6;
+
+
+// ============= QUESTION 05
+
+// function toss() {
+//     let toss = Math.floor(Math.random() * 2 ) + 1 
+//     if (toss == "1 ") {
+//         document.getElementById('toss').innerText = "head"
+//     } else if (toss == "2") {
+//         document.getElementById('toss').innerText = "tail"
+
+//     }
+
+// }
+
+
+// ============= QUESTION 06
+
+// let num = Math.floor(Math.random() * 100) + 1
+// console.log(num)
+
+
+// ============= QUESTION 08
+
+// let secretNum = Math.floor(Math.random() * 10) + 1 ;
+// console.log(secretNum)
+// let user_trys = prompt("write a number between 1 to 10")
+
+// if (user_trys == secretNum) {
+//     alert("congrats")
+// } else if (user_trys == secretNum + 1 || user_trys == secretNum - 1 ) {
+//     alert("you are very close.")
+// } else {
+//     alert("sorry")
+// }
+
